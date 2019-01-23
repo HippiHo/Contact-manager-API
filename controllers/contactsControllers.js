34de0db986e2b58b67d665d286408ed6038a6b35
Elise Maschke <elise.maschke@outlook.de>
2019-01-23 16:03:55 +0100

    Added error handling for contact controller

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,28 +1,95 @@
 const Contact = require("../model/Contact");
 
 exports.listContacts = async (req, res) => {
-  res.json(await Contact.find());
+  try {
+    res.json(await Contact.find());
+  } catch (e) {
+    console.error(e);
+  }
 };
 
-exports.addContact = async (req, res) => {
-  const contact = new Contact(req.body);
-  await contact.save();
-  res.json(contact);
+exports.addContact = async (req, res, next) => {
+  try {
+    const contact = new Contact(req.body);
+    await contact.save();
+    res.json(contact);
+  } catch (e) {
+    // console.dir(e);
+    if (e.name === "ValidationError") {
+      e.statusCode = 422;
+      next(e);
+    } else {
+      throw e;
+    }
+  }
 };
 
 exports.getContact = async (req, res) => {
-  const contact = await Contact.findById(req.params.id);
-  res.json(contact);
+  try {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) throw new Error("Not found");
+
+    res.json(contact);
+  } catch (e) {
+    if (
+      (e.name === "CastError" && e.path === "_id") ||
+      e.message === "Not found"
+    ) {
+      e.statusCode = 404;
+      e.message = "Contact not found";
+      next(e);
+    } else {
+      throw e;
+    }
+  }
 };
 
-exports.updateContact = async (req, res) => {
-  const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, {
-    new: true
-  });
-  res.json(contact);
+exports.updateContact = async (req, res, next) => {
+  try {
+    const contact = await Contact.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    res.json(contact);
+  } catch (e) {
+    // console.dir(e);
+    if (e.name === "ValidationError") {
+      e.statusCode = 422;
+      next(e);
+    } else if (
+      (e.name === "CastError" && e.path === "_id") ||
+      e.reason.name === "ObjectParameterError"
+    ) {
+      e.statusCode = 404;
+      e.message = "Contact not found";
+      next(e);
+    } else if (e.name === "CastError") {
+      next(e);
+    } else {
+      throw e;
+    }
+  }
 };
 
-exports.deleteContact = async (req, res) => {
-  const contact = await Contact.findByIdAndDelete(req.params.id);
-  res.json(contact);
+exports.deleteContact = async (req, res, next) => {
+  try {
+    const contact = await Contact.findByIdAndDelete(req.params.id);
+    if (!contact) throw new Error("Not found");
+    // wrong id
+    res.json(contact);
+  } catch (e) {
+    if (e.message === "Not found") {
+      // book already deleted
+      e.statusCode = 404;
+      e.message = "Contact already deleted.";
+      next(e);
+    } else if (e.name === "CastError" && e.kind === "ObjectId") {
+      // not selected by ID
+      e.statusCode = 404;
+      e.message = "Please select by ID";
+      next(e);
+    } else {
+      console.error(e);
+    }
+  }
 };
